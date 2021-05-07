da72a949e0001fffeb42ad49ea0f5080d8c75b96
cesar puentes <caps2596@gmail.com>
2021-05-07 16:47:26 -0500

    :construction: Trabajo en clase

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,16 +4,194 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Color(0xff0f0f0),
+      backgroundColor: Colors.white70,
       appBar: AppBar(
         title: Text("Mi pagina principal"),
       ),
       body: Center(
-        child: Text("Hola Mundo!!!"),
+        child: Column(
+          children: [
+            Container(
+              width: 300,
+              height: 100,
+              alignment: Alignment.center,
+              //color: Colors.black12,
+              margin: EdgeInsets.only(left: 5, top: 25, right: 15, bottom: 10),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              decoration: BoxDecoration(
+                //color: Colors.black12,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(40),
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(55)
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.redAccent,
+                        blurRadius: 40,
+                        offset: Offset(20, 20)
+                    )
+                  ],
+                  gradient: LinearGradient(
+                      colors: [
+                        Colors.blue,
+                        Colors.red,
+                        Colors.green
+                      ]
+                  )
+              ),
+              child: Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 2,
+                    //decoration: TextDecoration.combine([TextDecoration.overline,TextDecoration.underline])
+                    decoration: TextDecoration.none,
+                    height: 1.5,
+                    shadows:[
+                      Shadow(
+                          color: Colors.black45,
+                          offset: Offset(3,3),
+                          blurRadius: 2
+                      ),
+                      Shadow(
+                          color: Colors.red,
+                          offset: Offset(3,3),
+                          blurRadius: 2
+                      )
+                    ]
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 100,
+              alignment: Alignment.center,
+              //color: Colors.black12,
+              margin: EdgeInsets.only(left: 5, top: 25, right: 15, bottom: 10),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              decoration: BoxDecoration(
+                //color: Colors.black12,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(40),
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(55)
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.redAccent,
+                        blurRadius: 40,
+                        offset: Offset(20, 20)
+                    )
+                  ],
+                  gradient: LinearGradient(
+                      colors: [
+                        Colors.blue,
+                        Colors.red,
+                        Colors.green
+                      ]
+                  )
+              ),
+              child: Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 2,
+                    //decoration: TextDecoration.combine([TextDecoration.overline,TextDecoration.underline])
+                    decoration: TextDecoration.none,
+                    height: 1.5,
+                    shadows:[
+                      Shadow(
+                          color: Colors.black45,
+                          offset: Offset(3,3),
+                          blurRadius: 2
+                      ),
+                      Shadow(
+                          color: Colors.red,
+                          offset: Offset(3,3),
+                          blurRadius: 2
+                      )
+                    ]
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 100,
+              alignment: Alignment.center,
+              //color: Colors.black12,
+              margin: EdgeInsets.only(left: 5, top: 25, right: 15, bottom: 10),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              decoration: BoxDecoration(
+                //color: Colors.black12,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(40),
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(55)
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.redAccent,
+                        blurRadius: 40,
+                        offset: Offset(20, 20)
+                    )
+                  ],
+                  gradient: LinearGradient(
+                      colors: [
+                        Colors.blue,
+                        Colors.red,
+                        Colors.green
+                      ]
+                  )
+              ),
+              child: Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 2,
+                    //decoration: TextDecoration.combine([TextDecoration.overline,TextDecoration.underline])
+                    decoration: TextDecoration.none,
+                    height: 1.5,
+                    shadows:[
+                      Shadow(
+                          color: Colors.black45,
+                          offset: Offset(3,3),
+                          blurRadius: 2
+                      ),
+                      Shadow(
+                          color: Colors.red,
+                          offset: Offset(3,3),
+                          blurRadius: 2
+                      )
+                    ]
+                ),
+              ),
+            ),
+          ],
+        )
+
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
-        child: Icon(Icons.accessibility_sharp),
+        child: Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       drawer: Drawer(
